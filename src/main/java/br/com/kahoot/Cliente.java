643a6d25635c5f0c495cbe30ecec8ea96cb2581a
Samuel_Ralise <samuralise21@gmail.com>
2026-06-10 21:29:22 -0300

    fix: faz cliente ler todas as mensagens do servidor

--- src/main/java/br/com/kahoot/Cliente.java
+++ src/main/java/br/com/kahoot/Cliente.java
@@ -6,18 +6,18 @@ import java.net.Socket;
 
 public class Cliente {
     public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 12345);
+        try (Socket socket = new Socket("localhost", 12345);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream()))) {
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
+            String mensagem;
+            while ((mensagem = in.readLine()) != null) {
+                System.out.println("Servidor disse: " + mensagem);
 
-            String mensagem = in.readLine();
-            System.out.println("Servidor disse: " + mensagem);
-
-            socket.close();
-
+                if ("FIM".equals(mensagem)) {
+                    break;
+                }
+            }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
